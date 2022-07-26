fc56c3fa80a93dbbcb52f2fb42eb0a5bb1c58961
liversonp <liverson.p@gmail.com>
2022-07-25 21:44:08 -0300

    Questão solucionada

---
+++ MarkTheDustSweeper/MarktheDustSweeper.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int rooms;
+        int i=0;
+        cin >> rooms;
+        vector <int> salas(rooms);
+        long long int sum = 0;
+
+        for(int r=0;r<rooms;++r){
+            cin >> salas[r];
+        }
+        
+        while(i < rooms && salas[i] == 0){
+            i++;
+        }
+
+        for(int j=i; j<rooms-1;j++){
+            sum += salas[j];
+            if(salas[j] == 0){
+              sum++;
+            }
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
